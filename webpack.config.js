9f9ac1552769e1ee8ad5b3ed75b2a5cac57a5f69
Fram <fram@Frams-MacBook-Pro.local>
2019-08-30 14:59:31 +0700

    use html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+/**
+ * This plugin responsible for adding bundle js file to a specific template
+ * In case the bundle file name changed everytime compiling
+ * If you have any CSS assets in webpack's output (for example, CSS extracted 
+ *  with the MiniCssExtractPlugin) then these will be included with <link> tags 
+ *  in the <head> element of generated HTML.
+ */
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: './index.html',
+})
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: {main: './src/index.js'},
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'main.js',
   },
   module: {
     rules: [
@@ -12,9 +25,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
-};
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+}
